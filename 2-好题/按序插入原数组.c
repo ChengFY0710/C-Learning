4b0355b74c2dd832db3c2841916ea987162c1ca0
ChengFY-VSC <chengfy_0710@qq.com>
2025-11-02 22:18:01 +0800

    好题 - 优化：按序查找数组

--- 2-好题/按序插入原数组.c
+++ 2-好题/按序插入原数组.c
@@ -13,14 +13,11 @@ int main()
             break;
         }
     }
-    if(t==4)
-        num[5] = n;
-    else{
-        for(i=4; i>t; i--){
-            num[i+1] = num[i];
-        }
-        num[t+1] = n;
+
+    for(i=4; i>t; i--){
+        num[i+1] = num[i];
     }
+    num[t+1] = n;
     for(i=0; i<=5; i++){
         printf("%d", num[i]);
     }
